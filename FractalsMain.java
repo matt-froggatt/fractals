f0d2def4946b3c7298d452301fd335712e0106a4
Matthew Froggatt <you@your-domain.com>
2019-02-05 14:03:32 -0500

    Changed some length parameters to make things look cooler

--- FractalsMain.java
+++ FractalsMain.java
@@ -23,12 +23,12 @@ public class FractalsMain{
 			// Part of hacky fix, just sets the position of pos so that the same Point object can be used
 			// and then calls recursively to this method with new parameters.
 			pos.setLocation(x, y);
-			drawTreeRecurse(window, pos, length / 2, theta + (Math.PI / 4));
+			drawTreeRecurse(window, pos, (length * 2) / 3, theta + (Math.PI / 8));
 			
 			// Same as above. Positioning reset was required to avoid weird positioning issue caused by using the same
 			// Point object, since the previous positioning carries back through recursion.
 			pos.setLocation(x, y);			
-			drawTreeRecurse(window, pos, length / 2, theta - (Math.PI / 4));
+			drawTreeRecurse(window, pos, (length * 2) / 3, theta - (Math.PI / 8));
 
 			// Repaints the window to make sure the tree shows up.
 			window.repaint();
@@ -39,7 +39,7 @@ public class FractalsMain{
 	// be changed anyways, but I should create constants for them or something. Need to take in window so I can pass it from
 	// the main method and draw new lines in the recursive method.
 	public static void drawTree (LineWindow window){
-		drawTreeRecurse(window, new Point(window.getWidth() / 2, window.getHeight()), window.getHeight() / 2, Math.PI / 2);
+		drawTreeRecurse(window, new Point(window.getWidth() / 2, window.getHeight()), window.getHeight() / 4, Math.PI / 2);
 	}
 
 	public static void main(String[] args) {
